3bc7db16af5fdc1564fed316449e5e17f8fd35b7
VictorFil67 <victorfilinovych@gmail.com>
2024-06-13 02:26:34 +0300

    added user books list, delete user book,added StartReadingModal markup and styles

--- src/components/LibraryItem/LibraryItem.jsx
+++ src/components/LibraryItem/LibraryItem.jsx
@@ -1,42 +1,55 @@
 import { useDispatch } from "react-redux";
-import { LibraryItemImg } from "./LibraryItem.Styled";
+import {
+  Author,
+  BookItem,
+  Cover,
+  CoverText,
+  DeleteButton,
+  Description,
+  LibraryItemImg,
+  Span,
+  TextWrap,
+  Title,
+} from "./LibraryItem.Styled";
 import { deleteUserBook } from "../../store/books/operations";
-import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";
+import TrashCanSVG from "../../images/myLibraryBooksImages/TrashCanSVG";
+import { createPortal } from "react-dom";
+import { StartReadingModal } from "../StartReadingModal/StartReadingModal";
+import { useState } from "react";
+import { cutString } from "../../helpers/cutString";
 
 const LibraryItem = ({ book }) => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleDeleteBook = (id) => {
-    dispatch(deleteUserBook(id))
-      .unwrap()
-      .then(() => {
-        toast.success("Book deleted successfully!");
-      })
-      .catch((error) => {
-        toast.error("Error deleting book: " + error.message);
-      });
-  };
-
-  const handleFetchBookInfo = (id) => {
-    navigate(`/reading/${id}`);
-  };
+  const [modal, setModal] = useState(false);
 
   return (
-    <li>
-      <LibraryItemImg src={book.imageUrl} alt={book.title} />
-      <div>
-        <div>
-          <span>{book.title}</span>
-          <span>{book.author}</span>
-        </div>
-        <button onClick={() => handleDeleteBook(book._id)}>Delete</button>
-        <button onClick={() => handleFetchBookInfo(book._id)}>
-          Go to the selected book
-        </button>
-      </div>
-    </li>
+    <>
+      <BookItem>
+        <Cover $image={book.imageUrl} onClick={setModal}>
+          {book.imageUrl ? (
+            <LibraryItemImg src={book.imageUrl} alt={book.title} />
+          ) : (
+            <CoverText>
+              There is no cover for <br /> <Span>{book.title}</Span>
+            </CoverText>
+          )}
+        </Cover>
+        <Description>
+          <TextWrap>
+            <Title>{cutString(book.title, 16)}</Title>
+            <Author>{cutString(book.author, 20)}</Author>
+          </TextWrap>
+          <DeleteButton onClick={() => dispatch(deleteUserBook(book._id))}>
+            <TrashCanSVG />
+          </DeleteButton>
+        </Description>
+      </BookItem>
+      {modal &&
+        createPortal(
+          <StartReadingModal book={book} setModal={setModal} />,
+          document.body
+        )}
+    </>
   );
 };
 
